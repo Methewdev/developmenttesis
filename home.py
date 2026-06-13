3a15e777212bf919a9b0ccf33786f8ecef8f1d92
Methewdev <envidevelopment@gmail.com>
2026-06-13 21:47:49 +0700

    Update home.py

--- home.py
+++ home.py
@@ -1,6 +1,6 @@
 # app.py
 
-```python
+
 import streamlit as st
 import pandas as pd
 import torch
@@ -511,4 +511,3 @@ elif menu == "📁 Analisis Dataset":
                 "hasil_analisis.csv",
                 "text/csv"
             )
-```
